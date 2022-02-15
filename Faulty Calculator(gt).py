2d96bc8fc0eb5e5774df6d8cde15db2113538e9f
Gauarv Malpedi <gaurav.malpedi17@gmail.com>
2022-02-15 11:52:54 +0530

    Initial commit

---
+++ Faulty Calculator(gt).py
@@ -0,0 +1,51 @@
+# print("Welcome to the faulty calculator!!!!! \n "
+#       "Faulty calculations for 45*3, 56+9 and 56/8 \n "
+#       "Many faulty things can be done but I choose a minimum of 3 faulty calculations.")
+#
+# i = 0
+# while(True):
+#     i = i + 0
+#     yn = input("Enter 'y' to continue and 'n' to quit from this amazing calculator. \n")
+#     if yn == 'y':
+#         print("Let's get started!!!")
+#     elif yn == 'n':
+#         print("See ya!!!")
+#         break
+#     else:
+#         print("enter either y or n")
+#         continue
+#
+#     op = input("Enter the operator you wanna use")
+#     n1 = int(input("Enter the 1st number"))
+#     n2 = int(input("Enter the 2nd number"))
+#
+#     if op == "*" and n1 == 45 and n2 == 3:
+#         print("Your answer is 555")
+#     elif op == "+" and n1 == 56 and n2 == 9:
+#         print("Your answer is 77")
+#     elif op == "/" and n1 == 56 and n2 == 8:
+#         print("Your answer is 8")
+#
+#     elif op == "*":
+#         m = n1*n2
+#         print("Your answer is", m)
+#
+#     elif op == "/":
+#         d = n1/n2
+#         print("Your answer is", d)
+#
+#     elif op == "+":
+#         ad = n1+n2
+#         print("Your answer is", ad)
+#
+#     elif op == "-":
+#         su = n1-n2
+#         print("Your answer is", su)
+#
+#     elif op == "**":
+#         po = n1**n2
+#         print("Your answer is", po)
+#
+#     else:
+#         print("Didn't get you")
+#         continue
